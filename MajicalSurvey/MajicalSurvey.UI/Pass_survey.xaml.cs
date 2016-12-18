b2e37fb9c691ec963ce04ae8fec503f5072d8904
User <User@HP15W8>
2016-12-18 18:24:00 +0300

    UI is ready

--- MajicalSurvey/MajicalSurvey.UI/Pass_survey.xaml.cs
+++ MajicalSurvey/MajicalSurvey.UI/Pass_survey.xaml.cs
@@ -24,7 +24,7 @@ namespace MajicalSurvey.UI
     public partial class Pass_survey : Window
     {
 
-        
+
         IAnswerRepository ar;
         IQuestionRepository qr;
         IUsersRepository ur;
@@ -53,13 +53,18 @@ namespace MajicalSurvey.UI
 
         private void Next_Clicked(object sender, RoutedEventArgs e)
         {
-            foreach (RadioButton r in answer_stackpanel.Children) r.IsChecked = false;
+            foreach (RadioButton r in answer_stackpanel.Children)
+            {
+                r.IsChecked = false;
+                r.Content = null;
+            }
+
 
             k++;
             New_question();
 
             Save_answer();
-           
+
         }
         public void New_question()
         {
@@ -68,25 +73,26 @@ namespace MajicalSurvey.UI
             surveyName.Text = survey.Name; // Название опросника
 
             list_question = qr.GetAllQuestions(S.Name); // Присваивание вопросов
-            Number.Text = k.ToString();
+            Number.Text = (k+1).ToString();
 
             questionName.Text = (list_question[k]).Name; // Название вопроса
 
             list_answer = ar.GetAllAnswers((list_question[k]).Id)
-                .Where(x=>x.Question.Name == list_question[k].Name).ToList(); // Присваивание ответов
+                .Where(x => x.Question.Name == list_question[k].Name).ToList(); // Присваивание ответов
 
             for (int i = 0; i < list_answer.Count; i++) // Название ответов
             {
+                list_radio[i].Visibility = Visibility.Visible;
                 list_radio[i].Content = list_answer[i].RadioButtonName;
             }
 
-            Counter.Text = string.Format("It is {0} question from {1}", k, list_question.Count);
+            Counter.Text = string.Format("It is {0} question from {1}", k +1,
+                list_question.Count);
 
             if (k + 1 == list_question.Count)
             {
                 next.Visibility = Visibility.Collapsed;
                 Complete.Visibility = Visibility.Visible;
-
             }
 
         }
@@ -101,23 +107,24 @@ namespace MajicalSurvey.UI
 
         private void Complete_Clicked(object sender, RoutedEventArgs e)
         {
-        //    //отправка данных в бд
+            //    //отправка данных в бд
 
-        //    foreach (Answers a in list_answer) ar.Insert(a);
+            //    foreach (Answers a in list_answer) ar.Insert(a);
 
-        //    ChooseSurvey c = new ChooseSurvey();
-        //    var user = c.u; // создание пользователя
-        //    user.Answers = list_answer;
-        //    ur.Insert(user);
+            //    ChooseSurvey c = new ChooseSurvey();
+            //    var user = c.u; // создание пользователя
+            //    user.Answers = list_answer;
+            //    ur.Insert(user);
 
-        //    ur.Save();
+            //    ur.Save();
 
-        //    MessageBox.Show("Survey is passed", "Well done!");
-        //    Close();
+            //    MessageBox.Show("Survey is passed", "Well done!");
+            //    Close();
         }
 
         public void Save_answer()
         {
+            int m = 0;
             foreach (RadioButton r in answer_stackpanel.Children)
             {
                 if (r.IsChecked == true)
@@ -126,6 +133,14 @@ namespace MajicalSurvey.UI
                     a.RadioButtonName = r.Content.ToString();
                     a.Question = list_question[k];
                     user_list_answer.Insert(k, a);
+                }
+
+                else m++;
+
+                if (m == list_answer.Count)
+                {
+                    MessageBox.Show("You haven't chosen any answer", "Oops");
+                    return;
                 }
             }
         }
